38526dc04f1607d830b72f6b4605bf0f6362cb44
HyeonWooGa <yeonwoopark22@gmail.com>
2022-06-01 09:38:17 +0900

    #6.0 Introduction of Effect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("render"); // react code is whole re-rendered every time when states are changed by default. So we will learn useEffect
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Countinue"} />
+      <h1 className={styles.title}>{counter}</h1>
+      <Button onClick={onClick} text={"Countinue"} />
     </div>
   );
 }
