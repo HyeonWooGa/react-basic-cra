c06290b8e98415a4adbd94069e3056eb0b447dc7
HyeonWooGa <yeonwoopark22@gmail.com>
2022-06-01 14:28:16 +0900

    #6.3 Recap

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ function App() {
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
-  console.log("I run all te time"); // react code is whole re-rendered every time when states are changed by default. So we will learn useEffect to use api etc something that need to render only once.
-  useEffect(() => console.log("CALL THE API...."), []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5)
-      console.log("SEARCH FOR", keyword);
-  }, [keyword]);
+  console.log("I run all the time"); // react code is whole re-rendered every time when states are changed by default. So we will learn useEffect to use api etc something that need to render only once.
+  useEffect(() => console.log("I run only once."), []);
+  useEffect(() => console.log("I run when 'keyword' changes."), [keyword]);
+  useEffect(() => console.log("I run when 'counter' changes."), [counter]);
+  useEffect(
+    () => console.log("I run when 'keyword' or 'counter changes."),
+    [keyword, counter]
+  );
   return (
     <div>
       <input
