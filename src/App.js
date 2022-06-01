e736126e35dd17711f643a8109a7b49a0791ad2e
HyeonWooGa <yeonwoopark22@gmail.com>
2022-06-02 00:24:41 +0900

    #7.2 Coin Tracker (+code challenge)

--- src/App.js
+++ src/App.js
@@ -1,41 +1,51 @@
 import { useState, useEffect } from "react";
+
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [usd, setUsd] = useState(0);
+  const regex = /[+-]?\d+(\.\d+)?/g;
   const onChange = (event) => {
-    setToDo(event.target.value);
+    setPrice(event.target.value.match(regex));
+    setName(event.target.value.replace(/ .*/, ""));
   };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]); // "..." give us Elements of Array
-    setToDo("");
+  const onInput = (event) => {
+    setUsd(event.target.value);
   };
-  useEffect(() => console.log(toDos), [toDos]);
-  useEffect(
-    () => console.log(toDos.map((item, index) => <li key={index}>{item}</li>)),
-    [toDos]
-  );
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setPrice(json[0].quotes.USD.price);
+        setName("Bitcoin");
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onChange}>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      {loading ? null : (
+        <div>
+          <input id="usd" value={usd} onInput={onInput} />
+          <label htmlFor="usd"> USD</label>
+          <input id="coin" value={Math.round((usd / price) * 100) / 100} />
+          <label htmlFor="coin"> {name}</label>
+        </div>
+      )}
     </div>
   );
 }
