15a0fc29c7265c17539466d449507b13946fe92d
HyeonWooGa <yeonwoopark22@gmail.com>
2022-06-03 18:22:06 +0900

    #7.5 React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
@@ -8,3 +26,7 @@ export default App;
 
   In React Way, We never do state = "value" like this, We must use modifier when change value of state.
 */
+/*
+  Route : URL,
+  Switch : find Route
+ */
